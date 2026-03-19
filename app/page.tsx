08a28e75664dab5557c9c7e991880f5f8c2d89de
Sean <49212263+permasean@users.noreply.github.com>
2026-03-19 11:01:06 -0400

    use env var for API base url

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import GraphViewer from "@/components/GraphViewer";
 import { ActionBlueprintGraph } from "@/types/graph";
 
 export default async function Home() {
-  const res = await fetch('http://localhost:3000/api/v1/tenant1/actions/blueprints/bp_01jk766tckfwx84xjcxazggzyc/graph', {
+  const res = await fetch(`${process.env.API_BASE_URL}/api/v1/tenant1/actions/blueprints/bp_01/graph`, {
     cache: 'no-store'
   });
   const data: ActionBlueprintGraph = await res.json();
